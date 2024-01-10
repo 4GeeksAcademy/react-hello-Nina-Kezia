fe24453b200d41f4498d56bcc3a93f9be700ef6d
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-01-10 00:09:12 +0000

    React-Nina-Kezia

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,12 +8,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	return (
 	<div className="whole-wheat">
-		<div class="container">
-  			<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    		<div class="container-fluid">
-    		<a class="navbar-brand" href="#">Navbar</a>
+		<div className="container">
+  			<nav className="navbar navbar-expand-lg bg-body-tertiary">
+    		<div className="container-fluid">
+    		<a className="navbar-brand" href="#">Navbar</a>
     		</div>
   			</nav>
+		</div>
+
+		<div className="jumbotron">
+  			<h1 className="display-4">Hello, world!</h1>
+  				<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  					<hr className="my-4"/>
+ 				<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  				<p className="lead">
+    				<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  				</p>
 		</div>
 		<div>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
