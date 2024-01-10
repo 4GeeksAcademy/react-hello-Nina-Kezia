591504add0bfd502327de52f6ed577cfe464eb82
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-01-10 05:42:11 +0000

    Changed made to jumbo tron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import { propTypes } from "react-bootstrap/esm/Image"; /*may be able to delete*/
 const Jumbotron = (props) => {
 	return (
-	// 	<div className="jumbotron">
-    //   <h1 className="display-4">Hello, world!</h1>
-    //   <p className="lead">
-    //     This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
-    //   </p>
-    //   <hr className="my-4" />
-    //   <p>
-    //     It uses utility classes for typography and spacing to space content out within the larger container.
-    //   </p>
-    //   <p className="lead">
-    //     <a className="btn btn-primary btn-lg" href="#" role="button">
-    //       Learn more
-    //     </a>
-    //   </p>
-    // </div>
+	 	<div className="jumbotron p-3 text-dark">
+       <h1 className="display-4">A Warm Welcome!</h1>
+       <p className="lead">
+         Peruse our selection of delectable recipes. You will find a complete break down 
+       </p>
+       {/* <hr className="my-4" /> */}
+       <p className="lead">
+         <a className="btn btn-primary btn-lg" href="#" role="button">
+           Learn more
+         </a>
+       </p>
+     </div>
 	);
 
 };
