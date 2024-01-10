31e5d8e09e41b7d3c06b19b4e8cf94cbef5a65e4
NForlini <108763722+NForlini@users.noreply.github.com>
2024-01-10 00:07:55 +0000

    added new features

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { propTypes } from "react-bootstrap/esm/Image";
+
 const Card = (props) => {
 	return (
         <div className="card">
 			<div className="card">
-  				<img src={props.img} className="card-img-top" alt="..."/>
+  				<img src={props.img} className="card-img-top" alt="..." width={props.width} height={props.height}/>
   			<div className="card-body">
    				 <h5 className="card-title">{props.title}</h5>
-    			<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    			<a href="#" className="btn btn-primary">Go somewhere</a>
+    			<p className="card-text">{props.text}</p>
   			</div>
 		</div>
 		</div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
 		<Navbar/>
 		<Jumbotron/>
 		<div className="d-flex">
-			<Card title ={"food"}/>
-			<Card title ={"food2"}/>
-			<Card title ={"food3"}/>
-			<Card title ={"food4"}/>
+			<Card title ={"Global cooking at home"} img = {"https://canadianfoodfocus.org/wp-content/uploads/2021/03/cultural-cuisine-1024x576.jpg"} height={325} width = {500} text = {"Find your passion at home!"}/>
+			<Card title ={"Spices"} img = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfxu4CUdUMeYb29kWllHjig0WF5x1Fqr9waziol3-FInQ9U_TaDJMfcSUKMIiVNahFBy0&usqp=CAU"} height={325} width = {500} text = {"Learn to season properly"}/>
+			<Card title ={"Steak au Poivre"} img = {"https://cdn.britannica.com/14/226814-050-F3672D53/Wagyu-beef-ribeye-steak-grilled.jpg"} height={325} width = {500} text = {"The classic French dish"}/>
+			<Card title ={"New recipes"} img = {"https://welovefire.com/wp-content/uploads/2022/07/Cuisine-du-monde-_-Essayez-de-nouvelles-recettes-cet-ete%E2%80%89-copy-scaled.jpg"} height={325} width = {500} text = {"New recipes and techniques to learn"}/>
 		</div>
 		<Footer/>
 	</div>
