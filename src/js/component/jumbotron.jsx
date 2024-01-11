48b229b18b06c4554b270258b2a0c6d4d7c696df
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-01-11 00:44:19 +0000

    Background Color for Jumbotron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -5,11 +5,11 @@ const Jumbotron = (props) => {
 	 	<div className="jumbotron p-3 text-dark">
        <h1 className="display-4">A Warm Welcome!</h1>
        <p className="lead">
-         Peruse our selection of delectable recipes. You will find a complete break down of our recipe selection, just click the button. You know you want to!
+         Peruse our selection of delectable recipes. You will find a complete break down of our recipe selection, just click the button. You know you want to.
        </p>
        {/* <hr className="my-4" /> */}
        <p className="lead">
-         <a className="btn btn-primary btn-lg" href="#" role="button">Learn more
+         <a className="btn btn-primary btn-lg" href="#" role="button">Click me!
          </a>
        </p>
      </div>
