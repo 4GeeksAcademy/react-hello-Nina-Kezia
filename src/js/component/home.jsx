5d29811d17dc887f985f8d3cd65ccf31835c24c2
NForlini <108763722+NForlini@users.noreply.github.com>
2024-01-11 01:26:59 +0000

    css update

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,14 +10,11 @@ const Home = () => {
     <div className="whole-wheat"> 
         <Navbar/>
         <Jumbotron/>
-
-
-        <div className="d-flex flex-wrap justify-content-center"> 
-            <Card title ={"Global cooking at home"} img = {"https://canadianfoodfocus.org/wp-content/uploads/2021/03/cultural-cuisine-1024x576.jpg"} height={"325px"} width = {"500px"} text = {"Find your passion at home!"}/>
-            <Card title ={"Spices"} img = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfxu4CUdUMeYb29kWllHjig0WF5x1Fqr9waziol3-FInQ9U_TaDJMfcSUKMIiVNahFBy0&usqp=CAU"} height={"325px"} width = {"500px"} text = {"Learn to season properly"}/>
-            <Card title ={"Steak au Poivre"} img = {"https://cdn.britannica.com/14/226814-050-F3672D53/Wagyu-beef-ribeye-steak-grilled.jpg"} height={"325px"} width = {"500px"} text = {"The classic French dish"}/>
-            <Card title ={"New recipes"} img = {"https://welovefire.com/wp-content/uploads/2022/07/Cuisine-du-monde-_-Essayez-de-nouvelles-recettes-cet-ete%E2%80%89-copy-scaled.jpg"} height={"325px"} width = {"500px"} text = {"New recipes and techniques to learn"}/>
-
+        <div id="cards" className="d-flex flex-wrap justify-content-center"> 
+            <Card title ={"Global cooking at home"} img = {"https://canadianfoodfocus.org/wp-content/uploads/2021/03/cultural-cuisine-1024x576.jpg"} height={"225px"} width = {"400px"} text = {"Find your passion at home!"}/>
+            <Card title ={"Spices"} img = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfxu4CUdUMeYb29kWllHjig0WF5x1Fqr9waziol3-FInQ9U_TaDJMfcSUKMIiVNahFBy0&usqp=CAU"} height={"225px"} width = {"400px"} text = {"Learn to season properly"}/>
+            <Card title ={"Steak au Poivre"} img = {"https://cdn.britannica.com/14/226814-050-F3672D53/Wagyu-beef-ribeye-steak-grilled.jpg"} height={"225px"} width = {"400px"} text = {"The classic French dish"}/>
+            <Card title ={"New recipes"} img = {"https://welovefire.com/wp-content/uploads/2022/07/Cuisine-du-monde-_-Essayez-de-nouvelles-recettes-cet-ete%E2%80%89-copy-scaled.jpg"} height={"225px"} width = {"400px"} text = {"New recipes and techniques to learn"}/>
         </div>
         <Footer/>
     </div>
