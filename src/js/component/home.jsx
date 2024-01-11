7153094dbc94189590501316c8d4cc6669435269
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-01-11 00:36:20 +0000

    Added Margin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,11 +10,7 @@ const Home = () => {
     <div className="whole-wheat">
         <Navbar/>
         <Jumbotron/>
-<<<<<<< HEAD
         <div className="d-flex">
-=======
-        <div className="d-flex mx-auto my-3"> 
->>>>>>> d6e7539bb64f41adbd76a6282cdb7df9d8cf3453
             <Card title ={"Global cooking at home"} img = {"https://canadianfoodfocus.org/wp-content/uploads/2021/03/cultural-cuisine-1024x576.jpg"} height={325} width = {500} text = {"Find your passion at home!"}/>
             <Card title ={"Spices"} img = {"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfxu4CUdUMeYb29kWllHjig0WF5x1Fqr9waziol3-FInQ9U_TaDJMfcSUKMIiVNahFBy0&usqp=CAU"} height={325} width = {500} text = {"Learn to season properly"}/>
             <Card title ={"Steak au Poivre"} img = {"https://cdn.britannica.com/14/226814-050-F3672D53/Wagyu-beef-ribeye-steak-grilled.jpg"} height={325} width = {500} text = {"The classic French dish"}/>
